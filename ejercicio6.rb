2ab8610c68f2c1f07795be7596850f371fcf4a87
ErickLedesma <ledesma.erick@gmail.com>
2018-11-06 21:26:08 -0300

    Deliver

--- ejercicio6.rb
+++ ejercicio6.rb
@@ -1,17 +1,43 @@
-# Se tiene el archivo config.ru :
-#
-# Se pide:
-# - Agregar un código de respuesta 200.
-# - Agregar en los Response Headers un Content-type de tipo text/html.
-# - Agregar en el Response Body una etiqueta de párrafo que contenga un texto Lorem
-#   ipsum.
+require 'faker'
+module Test
+    def result
+        average = (@grade1 + @grade2) / 2
+        puts "Persona: #{@alumn}"
+        if average > 4
+            puts 'Estudiante aprobado'
+        else   
+            puts 'Estudiante reprobado'
+        end
+    end 
+end 
 
-# config.ru
-require 'rack'
-class MiPrimeraWebApp
-    def call(env)
-        [200, {'Content-Type' => 'text/plain'}, ['<p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, molestiae blanditiis maxime repellat deserunt optio id suscipit reiciendis iure laudantium porro ab expedita deleniti sed nesciunt nulla aliquam sunt aliquid dolore, repudiandae neque. Deleniti voluptas nisi libero ab neque veniam, optio autem odit eius quaerat iusto distinctio reprehenderit, illo ducimus. </p>']]
+module Attendance
+    def student_quantity
+        student_qty
+    end
+end 
+
+class Student
+    attr_accessor :alumn, :grade1, :grade2
+    @@student_quantity = 0
+    @student_qty = 0
+    def initialize(alumn, grade1, grade2)
+        @alumn = alumn
+        @grade1 = grade1
+        @grade2 = grade2
+        @@student_quantity += 1
+    end 
+    include Test
+    extend  Attendance
+    def self.student_qty
+        @@student_quantity
     end
 end
-run MiPrimeraWebApp.new
 
+(1..10).each do
+    r = Random.new
+    student = Student.new(Faker::Simpsons.character, r.rand(1..7), r.rand(1..7))
+    puts student.result
+end 
+
+puts "Cantidad de alumnos #{Student.student_quantity}"
